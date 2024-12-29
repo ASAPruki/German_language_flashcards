d5dd328619831cd94f959e9b96450fae8efb8758
Mohamad Harouki <130186237+ASAPruki@users.noreply.github.com>
2024-12-29 19:39:24 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
-import 'package:flutter/material.dart';
-import 'flashcards.dart'; // Import the flashcards data
-import 'flashcards_screen.dart'; // Import the flashcard screen
+// The api url is hosted on awardspace: 'http://mohamadharouki.atwebpages.com/API.php';
+// 2 actions are in the API: random (gets a random word and its translation from the database), and add (posts a word and its translation to the database).
 
-void main() {
-  shuffleFlashcards(); // Shuffle cards before starting the app
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'German Language Flashcards',
       debugShowCheckedModeBanner: false,
-      title: 'Language Flashcards',
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.blueGrey,
         scaffoldBackgroundColor: const Color(0xFF121212),
         cardColor: const Color(0xFF1E1E1E),
       ),
-      home: const FlashcardScreen(),
+      home: HomeScreen(),
     );
   }
 }
